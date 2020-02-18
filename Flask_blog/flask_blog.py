bafac26d58cec32c9dfcff6819d2291a5d140023
McRhys189 <muragerhys189@gmail.com>
2020-02-18 21:29:06 +0300

    Resolving refresh error

--- Flask_blog/flask_blog.py
+++ Flask_blog/flask_blog.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1>Home page</h1>"
+    return "Hello, World!.. it is nice to see you"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 # commit
