8448d75e4fc008b4d2315c204c7bc1444ce360a1
McRhys189 <muragerhys189@gmail.com>
2020-02-23 22:36:15 +0300

    Forms and user input_day 1

---
+++ Flask_blog/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[
+        DataRequired(), Length(min=2, max=20)
+    ])
+    email = StringField("email", validators=[
+        DataRequired(), Email()
+    ])
+    password = StringField("Password", validators=[
+        DataRequired()])
+    
